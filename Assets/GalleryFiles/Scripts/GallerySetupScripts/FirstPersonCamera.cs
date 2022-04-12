c7a30c08d8ce5a6099290c26fc7df38a70e294b5
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2022-04-12 14:31:40 -0700

    Added ConsoleToGUI script

--- Assets/GalleryFiles/Scripts/GallerySetupScripts/FirstPersonCamera.cs
+++ Assets/GalleryFiles/Scripts/GallerySetupScripts/FirstPersonCamera.cs
@@ -40,7 +40,7 @@ public class FirstPersonCamera : MonoBehaviour
                         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
                         //playerBody.Rotate(Vector3.up * mouseX);
                         m_ASLObject.SendAndIncrementLocalRotation(Quaternion.Euler(0f, mouseX, 0f));
-                        Debug.Log("Sent Local Rotation");
+                        //Debug.Log("Sent Local Rotation");
                     });
 
         }
