4f7a92452a97ff63620fca2ba1a4aca32d087885
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2022-04-18 03:43:42 -0700

    Added new models for players

--- Assets/GalleryFiles/PavelDemo/Scripts/Old Unused Script/Pavel_Player.cs
+++ Assets/GalleryFiles/PavelDemo/Scripts/Old Unused Script/Pavel_Player.cs
@@ -85,6 +85,8 @@ public class Pavel_Player : MonoBehaviour
         // Delete player model in space
         gameObject.GetComponent<ASL.ASLObject>().SendAndSetClaim(() =>
         {
+            
+            
             GetComponent<ASL.ASLObject>().DeleteObject();
             manager.DisconnectFromServer();
             Application.Quit();
