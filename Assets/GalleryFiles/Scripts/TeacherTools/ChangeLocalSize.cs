e3f9cdda52621f3aa0218f914fabbb8b33d91c19
tydmiller <70169338+tydmiller@users.noreply.github.com>
2022-05-25 18:14:16 -0700

    Made student canvas smaller for teacher

---
+++ Assets/GalleryFiles/Scripts/TeacherTools/ChangeLocalSize.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ASL;
+
+public class ChangeLocalSize : MonoBehaviour
+{
+	GameObject[] studentArray;
+	static GameLiftManager gameManager;
+	// Start is called before the first frame update
+	void Start()
+	{
+		gameManager = GameObject.Find("GameLiftManager").GetComponent<GameLiftManager>();
+		studentArray = GameObject.FindGameObjectsWithTag("StudentPlayer");
+		if (gameManager.m_PeerId == gameManager.GetLowestPeerId())
+		{
+			for (int i = 0; i < studentArray.Length; i++)
+			{
+				GameObject canvas = studentArray[i].transform.GetChild(1).gameObject;
+				canvas.transform.localScale = new Vector3(canvas.transform.localScale.x / 2, canvas.transform.localScale.y / 2, canvas.transform.localScale.z / 2);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
