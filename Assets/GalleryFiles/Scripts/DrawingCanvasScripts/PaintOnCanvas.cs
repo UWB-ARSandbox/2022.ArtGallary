e35999303522bd271e7471ffdfa2aafcccc192e5
tydmiller <70169338+tydmiller@users.noreply.github.com>
2022-05-31 19:44:45 -0700

    fixed null exception bug on text typing

--- Assets/GalleryFiles/Scripts/DrawingCanvasScripts/PaintOnCanvas.cs
+++ Assets/GalleryFiles/Scripts/DrawingCanvasScripts/PaintOnCanvas.cs
@@ -939,7 +939,8 @@ public class PaintOnCanvas : MonoBehaviour
 			eraseTog.isOn = false;
 
 			GameObject.Find("TextInput").GetComponent<InputField>().interactable = true;
-			GameObject.Find("SaveField").GetComponent<InputField>().interactable = false;
+			//this used to be on the UI but now its in the menu so maybe keep it around
+			//GameObject.Find("SaveField").GetComponent<InputField>().interactable = false;
 
 			canLoad = false;
 			canSave = false;
